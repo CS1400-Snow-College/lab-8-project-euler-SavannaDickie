62c57efabf00f334a67a7bcad61ec5bedfa3c829
Savanna Dickie <personal3@SavannaMac.local>
2024-11-06 11:50:07 -0700

    explanation and menu

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@
 //Savanna Dickie
 // 11/05/2024
 // Lab 8: Project Euler
+Console.Clear();
+Console.WriteLine("\nThis is a program that will demostrate the solution to two Project Euler problems");
+Console.WriteLine("\n=== Projet Euler Menu ===\n\n1. Even Fibonacci Numbers\n2. Smallest Multiple\n3. Largest Prime Factor\n4. Summation of Primes");
+Console.Write("\nEnter the number of your selection ");
+Console.ReadLine();
